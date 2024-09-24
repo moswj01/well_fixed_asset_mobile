d2f8f90ae672fed16fa7ea08cb172dbc112b5cf9
Mos WJ <mosv.chara@gmail.com>
2024-09-25 00:03:00 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/well_fixed_asset_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.well_fixed_asset_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app/modules/home/views/home_view.dart
@@ -0,0 +1,144 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class HomeView extends GetView<HomeController> {
+  const HomeView({super.key});
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0xFFEEFFF0),
+            Color(0xFFE0E0E0),
+            Color(0xFFE5FFEA),
+          ]),
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 100),
+              child: Column(
+                children: [
+                  Image.asset(
+                    "assets/logos.png",
+                    width: 120,
+                  ),
+                  Text(
+                    "Well-Fixed Asset Mobile Application",
+                    style: TextStyle(
+                        color: Color(0xFF1E4E28),
+                        fontSize: 38,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  Platform.isAndroid
+                      ? Container(
+                          width: size.width,
+                          padding: EdgeInsets.only(right: 35),
+                          child: Text(
+                            "Android",
+                            style: TextStyle(
+                                color: Color(0xFF1E4E28), fontSize: 16),
+                            textAlign: TextAlign.right,
+                          ))
+                      : Container(
+                          width: size.width,
+                          padding: EdgeInsets.only(right: 35),
+                          child: Text(
+                            "iOS",
+                            style: TextStyle(
+                                color: Color(0xFF1E4E28), fontSize: 16),
+                            textAlign: TextAlign.right,
+                          )),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Column(
+                children: [
+                  Text(
+                    "Sign in to continue",
+                    style: TextStyle(
+                        color: Color(0xFF1E4E28),
+                        fontSize: 16,
+                        fontWeight: FontWeight.w300),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 45,
+                    width: size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12),
+                      
+                        color: Color(0xffffffff)),
+                    child: TextField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Color(0xFF1E4E28),),borderRadius: BorderRadius.circular(12)),
+                          hintText: "Username",
+                          contentPadding: EdgeInsets.all(10),
+                          hintStyle: TextStyle(color: Color(0xEF868686)),
+                          prefixIcon: Icon(Icons.person_rounded)),
+                    ),
+                  ),
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                      width: size.width,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF167031),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: TextButton.icon(
+                          onPressed: () {},
+                          label: Text(
+                            "Login",
+                            style: TextStyle(
+                                fontSize: 22, color: Color(0xffffffff)),
+                          ))),
+                  Container(
+                      margin:
+                          EdgeInsets.symmetric(horizontal: 30, ),
+                      width: size.width,
+                      height: 45,
+                      decoration: BoxDecoration(
+                          color: Color(0xFFFFD900),
+                          borderRadius: BorderRadius.circular(12)),
+                      child: TextButton.icon(
+                          onPressed: () {},
+                          label: Text(
+                            "Setting",
+                            style: TextStyle(
+                                fontSize: 22, color: Color(0xffffffff)),
+                          ))),
+                 
+                ],
+              ),
+            ),
+             Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+                    width: size.width,
+                    child: Text("www.wellfixedasset.com",textAlign: TextAlign.center,),
+                  )
+          ],
+        ),
+      ),
+    );
+  }
+}
